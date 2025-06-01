57216e6c9ac9a35175b998c029d280a2638819b5
kembiri <kembiri1@gmail.com>
2025-06-01 02:43:32 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,14 @@
-    const container = document.getElementById("movie-container");
-    const modal = document.getElementById("movieModal");
-    const iframe = document.getElementById("movieIframe");
-    const closeBtn = document.querySelector(".close");
+localStorage.setItem('filmData', JSON.stringify(filmData));
 
-    movies.forEach(movie => {
-      const card = document.createElement("div");
+    const container = document.getElementById("movie-container");
+
+    filmData.forEach(film => {
+      const card = document.createElement("a");
       card.className = "movie-card";
+      card.href = `player.html?slug=${encodeURIComponent(film.slug)}`;
       card.innerHTML = `
-        <img src="${movie.image}" alt="${movie.title}" />
-        <span>${movie.title}</span>
+        <img src="${film.image}" alt="${film.title}" />
+        <span>${film.title}</span>
       `;
-      card.addEventListener("click", () => {
-        iframe.src = movie.url;
-        modal.style.display = "flex";
-      });
       container.appendChild(card);
     });
-
-    closeBtn.onclick = function () {
-      modal.style.display = "none";
-      iframe.src = "";
-    };
-
-    window.onclick = function (event) {
-      if (event.target === modal) {
-        modal.style.display = "none";
-        iframe.src = "";
-      }
-    };
