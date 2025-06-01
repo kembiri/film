3c002471fef944feb12210f1ca62dcc2b268a5d6
kembiri <kembiri1@gmail.com>
2025-06-01 02:50:21 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-localStorage.setItem('filmData', JSON.stringify(filmData));
-
-    const container = document.getElementById("movie-container");
-
-    filmData.forEach(film => {
-      const card = document.createElement("a");
-      card.className = "movie-card";
-      card.href = `player.html?slug=${encodeURIComponent(film.slug)}`;
-      card.innerHTML = `
-        <img src="${film.image}" alt="${film.title}" />
-        <span>${film.title}</span>
-      `;
-      container.appendChild(card);
-    });
+    fetch('film.json')
+      .then(response => response.json())
+      .then(data => {
+        const allFilms = [...data.dramas, ...data.movies];
+        localStorage.setItem('filmData', JSON.stringify(allFilms));
+        renderList(data.dramas, 'drama-list');
+        renderList(data.movies, 'movie-list');
+      });
+  
+    function renderList(data, containerId) {
+      const container = document.getElementById(containerId);
+      data.forEach(item => {
+        const epsCount = item.episodes ? item.episodes.length : 1;
+        const poster = item.poster || "";
+        const link = document.createElement('a');
+        link.href = `player.html?slug=${encodeURIComponent(item.slug)}`;
+        link.innerHTML = `
+          <div class="list-anime">
+            <img src="${poster}" alt="${item.title}">
+            <p>${item.title}</p>
+            <span class="eps">${epsCount}</span>
+          </div>
+        `;
+        container.appendChild(link);
+      });
+    }
