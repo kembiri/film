f56e2cd3a43e944570d874960759719892fe9092
kembiri <kembiri1@gmail.com>
2025-07-08 03:58:03 -0700

    Update film-v2.js

--- film-v2.js
+++ film-v2.js
@@ -1260,4 +1260,15 @@ const filmData = [
       "https://about.vidio.com/wp-content/uploads/2022/07/SVOD-Shaolin-Soccer-Alt-A-Thumbnail.jpg",
     url: "https://firebasestorage.googleapis.com/v0/b/bangden-com.appspot.com/o/upload%2FD21%20FUN%20Shaolin%20soccer%202001%20mp4.mp4?alt=media&token=",
   },
+  {
+    title: "Kingdom of Heaven (2005)",
+    slug: "kingdom-of-heaven-2005",
+    type: "Movie",
+    genre: ["Action", "Adventure", "Drama"],
+    country: "Maroko",
+    poster: "https://image.tmdb.org/t/p/w500/rNaBe4TwbMef71sgscqabpGKsxh.jpg",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BMzdhNGZhMjQtYjVmNy00OWMyLTk1MmQtZGNhZDYwMzZlZjI3XkEyXkFqcGc@._V1_SX300.jpg",
+    url: "https://firebasestorage.googleapis.com/v0/b/bangden-com.appspot.com/o/upload%2FD21.FUN-Kingdom%20of%20Heaven%20(2005)-1080p.mp4?alt=media&token=e8d1eb06-0583-4892-8122-36c2b956201e",
+  },
 ];
