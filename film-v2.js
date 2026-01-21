283864fec86237d5192e80981d4023b28eb15878
kembiri <kembiri1@gmail.com>
2026-01-21 04:00:58 -0800

    Update film-v2.js

--- film-v2.js
+++ film-v2.js
@@ -1227,6 +1227,59 @@ const filmData = [
     ],
   },
   {
+    title: "My Boss - Season 1 (2024)",
+    slug: "my-boss-2024",
+    type: "Serial",
+    genre: ["Drama", "Comedy"],
+    country: "China",
+    poster:
+      "https://media.themoviedb.org/t/p/w260_and_h390_bestv2/KjrmpcxafFVm12TWNHEhD0ZBpu.jpg",
+    image:
+      "https://media.themoviedb.org/t/p/w260_and_h390_bestv2/KjrmpcxafFVm12TWNHEhD0ZBpu.jpg",
+    episodes: [
+      {
+        label: "Episode 1",
+        url: "https://playeriframe.netlify.app/?url=https://short.icu/KpngZsc6A",
+      },
+      {
+        label: "Episode 2",
+        url: "https://playeriframe.netlify.app/?url=https://short.icu/BVvBjeBps",
+      },
+      {
+        label: "Episode 3",
+        url: "https://playeriframe.netlify.app/?url=https://short.icu/x3ae-4QuG",
+      },
+      {
+        label: "Episode 4",
+        url: "https://playeriframe.netlify.app/?url=https://short.icu/vC46ucerV",
+      },
+      {
+        label: "Episode 5",
+        url: "https://playeriframe.netlify.app/?url=https://short.icu/Yp9ruIDt0",
+      },
+      {
+        label: "Episode 6",
+        url: "https://playeriframe.netlify.app/?url=https://short.icu/6KPEANyeS",
+      },
+      {
+        label: "Episode 7",
+        url: "https://playeriframe.netlify.app/?url=https://short.icu/9kZdxjjX9",
+      },
+      {
+        label: "Episode 8",
+        url: "https://playeriframe.netlify.app/?url=https://short.icu/ZMLyirpMH",
+      },
+      {
+        label: "Episode 9",
+        url: "https://playeriframe.netlify.app/?url=https://short.icu/3GuBm79YK",
+      },
+      {
+        label: "Episode 10",
+        url: "https://playeriframe.netlify.app/?url=https://short.icu/4rmKSUH24",
+      },  
+    ],
+  },
+  {
     title: "The Flu (2013)",
     slug: "the-flu-2013",
     type: "Movie",
